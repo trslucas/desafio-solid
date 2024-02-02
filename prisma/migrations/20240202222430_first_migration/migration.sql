3fbb6d53d7cd6da35c1e5f6b1efda7c0b6976ae0
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2024-02-02 19:26:25 -0300

    second commit

---
+++ prisma/migrations/20240202222430_first_migration/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Dog" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "energy" TEXT NOT NULL,
+    "independence" TEXT NOT NULL,
+    "environment" TEXT NOT NULL,
+
+    CONSTRAINT "Dog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
