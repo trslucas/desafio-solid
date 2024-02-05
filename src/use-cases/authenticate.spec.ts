9253ea298a41bdab71ddb5227853c1896eaf209b
Lucas Trindade <lucastrindade@MacBook-Air-de-Lucas.local>
2024-02-05 13:18:46 -0300

    feat: add authenticate use-case, and authenticate-use-case tests

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,62 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import { InMemoryOrgsRepository } from '../repository/in-memory/in-memory-orgs-repository'
+import { AuthenticateUseCase } from './authenticate'
+
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: AuthenticateUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new AuthenticateUseCase(orgsRepository)
+  })
+
+  it('should be able to authenticate', async () => {
+    await orgsRepository.create({
+      address: 'Avenida Nilo Peçanha',
+      cep: '25010189',
+      email: 'trslucas@outlook.com',
+      password_hash: await hash('Senha@2134', 6),
+      responsable_name: 'Lucas Trindade',
+      whatsapp: '21965667088',
+    })
+
+    const { org } = await sut.execute({
+      email: 'trslucas@outlook.com',
+      password: 'Senha@2134',
+    })
+
+    expect(org.id).toEqual(expect.any(String))
+  })
+
+  it('should be not able to authenticate with wrong email', async () => {
+    await expect(() =>
+      sut.execute({
+        email: 'trslucas@dasdasd.com',
+        password: 'Senha@2134',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await orgsRepository.create({
+      address: 'Avenida Nilo Peçanha',
+      cep: '25010189',
+      email: 'trslucas@thummi.com',
+      password_hash: await hash('Senha@2134', 6),
+      responsable_name: 'Lucas Trindade',
+      whatsapp: '21965667088',
+    })
+
+    await expect(() =>
+      sut.execute({
+        email: 'trslucas@thumi.com',
+        password: 'Senha@2134',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
